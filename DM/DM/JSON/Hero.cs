f62c50aa639f387183db394020f73528e1218cc1
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-05 21:12:09 +0100

    Reworked Hero class to get heroes list from API

--- DM/DM/JSON/Hero.cs
+++ DM/DM/JSON/Hero.cs
@@ -1,11 +1,10 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DM.JSON
 {
-    public class HeroObject
+    public class Hero
     {
         public int id { get; set; }
         public string name { get; set; }
@@ -14,6 +13,5 @@ namespace DM.JSON
         public string attack_type { get; set; }
         public List<string> roles { get; set; }
         public int legs { get; set; }
-        public string hero_avatar { get; set; }
     }
 }
