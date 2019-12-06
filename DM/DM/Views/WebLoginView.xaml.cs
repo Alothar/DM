664fe4a74d13516914162c54063b33c5dc8963c9
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-06 17:42:11 +0100

    Added new WebLoginView to enable login with OpenID

---
+++ DM/DM/Views/WebLoginView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DM.ID_handler;
+using DM.JSON;
+using DM.REST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebLoginView : ContentPage
+    {
+        private string current_url;
+
+        WebView webView = new WebView
+        {
+            Source = new UrlWebViewSource
+            {
+                Url = "https://api.opendota.com/login/",
+            },
+            VerticalOptions = LayoutOptions.FillAndExpand
+        };
+        public WebLoginView()
+        {
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
+
+            webView.Navigating += (object sender, WebNavigatingEventArgs e) =>
+            {
+                current_url = e.Url;
+                if (current_url.Contains("https://www.opendota.com/players/"))
+                {
+                    RestService rest = RestService.Instance;
+                    Id_holder.Instance.Steam32id = current_url.Split('/')[current_url.Split('/').Length - 1];
+                    ProfileObject player = rest.GetPlayerInfo(Id_holder.Instance.Steam32id);
+
+                    App.Current.MainPage.Navigation.PopAsync();
+                    App.Current.MainPage.Navigation.PushAsync(new WelcomePage(player));
+                }
+            };
+        }
+
+    }
+}
