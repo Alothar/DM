c8b3137bba1ec728047b7850931bd9fd6f2d72d0
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-13 21:13:49 +0100

    Reworked and optimised DB usage in this class

--- DM/DM/App.xaml.cs
+++ DM/DM/App.xaml.cs
@@ -8,6 +8,7 @@ using DM.JSON;
 using DM.REST;
 using System.Collections.Generic;
 using System.Linq;
+using DM.DB;
 
 namespace DM
 {
@@ -17,7 +18,7 @@ namespace DM
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new XF_LoginPage());
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
@@ -36,54 +37,39 @@ namespace DM
             // Handle when your app resumes
         }
 
-        [Table("Heroes")]
-        public class HeroesDB
-        {
-            [PrimaryKey, Column("ID")]
-            public int ID { get; set; }
-            [Column("Name")]
-            public string Name { get; set; }
-            [Column("Localized_name")]
-            public string Localized_name { get; set; }
-            [Column("Primary_attr")]
-            public string Primary_attr { get; set; }
-            [Column("Attack_type")]
-            public string Attack_type { get; set; }
-            [Column("Roles")]
-            public string Roles { get; set; }
-            [Column("Legs")]
-            public int Legs { get; set; }
-            [Column("Resource")]
-            public string Resource { get; set; }
-        }
+        
 
         public static void InitiateHeroesDB()
         {
             RestService rest = RestService.Instance;
             List<Hero> heroes = rest.GetHeroes();
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "HeroesSQLite.db3");
-            var db = new SQLiteConnection(dbPath);
-            db.CreateTable<HeroesDB>();
-            if (db.Table<HeroesDB>().Count() == 0)
+            using (SQLiteConnection db = new SQLiteConnection(dbPath))
             {
-                // only insert the data if it doesn't already exist
-                foreach (Hero hero in heroes)
+                db.CreateTable<HeroesDB>();
+                if (db.Table<HeroesDB>().Count() == 0)
                 {
-                    var newHero = new HeroesDB();
-                    newHero.ID = hero.id;
-                    newHero.Name = hero.name;
-                    newHero.Localized_name = hero.localized_name;
-                    newHero.Primary_attr = hero.primary_attr;
-                    newHero.Attack_type = hero.attack_type;
-                    newHero.Roles = "";
-                    foreach (string role in hero.roles)
+                    // only insert the data if it doesn't already exist
+                    foreach (Hero hero in heroes)
                     {
-                        newHero.Roles += role+" ";
+                        var newHero = new HeroesDB
+                        {
+                            ID = hero.id,
+                            Name = hero.name,
+                            Localized_name = hero.localized_name,
+                            Primary_attr = hero.primary_attr,
+                            Attack_type = hero.attack_type,
+                            Roles = ""
+                        };
+                        foreach (string role in hero.roles)
+                        {
+                            newHero.Roles += role + " ";
+                        }
+                        newHero.Legs = hero.legs;
+                        string[] splittedname = hero.name.Split(new String[] { "o_" }, StringSplitOptions.RemoveEmptyEntries);
+                        newHero.Resource = "DM.Resources.Heroes.app_assets_heroes_" + splittedname[splittedname.Length - 1] + "_full.png";
+                        db.Insert(newHero);
                     }
-                    newHero.Legs = hero.legs;
-                    string[] splittedname = hero.name.Split(new String[]{ "o_" }, StringSplitOptions.RemoveEmptyEntries);
-                    newHero.Resource = "DM.Resources.Heroes.app_assets_heroes_" + splittedname[splittedname.Length-1] + "_full.png";
-                    db.Insert(newHero);
                 }
             }
         }
@@ -93,11 +79,13 @@ namespace DM
             RestService rest = RestService.Instance;
             List<Hero> heroes = rest.GetHeroes();
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "HeroesSQLite.db3");
-            var db = new SQLiteConnection(dbPath);
-            var dbHeroArray = from hero in db.Table<HeroesDB>()
-                              select hero;
-            if (heroes.Count > dbHeroArray.Count())
-                db.DropTable<HeroesDB>();
+            using (SQLiteConnection db = new SQLiteConnection(dbPath))
+            {
+                var dbHeroArray = from hero in db.Table<HeroesDB>()
+                                  select hero;
+                if (heroes.Count > dbHeroArray.Count())
+                    db.DropTable<HeroesDB>();
+            }
             InitiateHeroesDB();
         }
 
