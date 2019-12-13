e2612aa61693f6b14560cf2ca1a35ae9a71c2e10
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-13 21:17:51 +0100

    Added new classes to handle new API responses

---
+++ DM/DM/JSON/MostPlayedHero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DM.JSON
+{
+    public class MostPlayedHero
+    {
+        public string hero_id { get; set; }
+        public int last_played { get; set; }
+        public int games { get; set; }
+        public int win { get; set; }
+        public int with_games { get; set; }
+        public int with_win { get; set; }
+        public int against_games { get; set; }
+        public int against_win { get; set; }
+    }
+}
---
+++ DM/DM/JSON/WL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DM.JSON
+{
+    public class WL
+    {
+        public int win { get; set; }
+        public int lose { get; set; }
+    }
+}
