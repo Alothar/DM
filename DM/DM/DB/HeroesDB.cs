0ee49cb8c41e5c3610f4d2550e36ce3e3320cc28
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-13 21:08:08 +0100

    Removed HeroesDB nested class from App.xaml.cs, created as separate class

---
+++ DM/DM/DB/HeroesDB.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DM.DB
+{
+    [Table("Heroes")]
+    public class HeroesDB
+    {
+        [PrimaryKey, Column("ID")]
+        public int ID { get; set; }
+        [Column("Name")]
+        public string Name { get; set; }
+        [Column("Localized_name")]
+        public string Localized_name { get; set; }
+        [Column("Primary_attr")]
+        public string Primary_attr { get; set; }
+        [Column("Attack_type")]
+        public string Attack_type { get; set; }
+        [Column("Roles")]
+        public string Roles { get; set; }
+        [Column("Legs")]
+        public int Legs { get; set; }
+        [Column("Resource")]
+        public string Resource { get; set; }
+    }
+}
