36211dc8fad189bc91fbb59a879bb7fe19198877
Sergey Omelchuk <sergomel52@gmail.com>
2019-12-17 19:37:48 +0100

    Renamed Profile page

--- DM/DM/ViewModels/ProfileViewModel.cs
+++ DM/DM/ViewModels/ProfileViewModel.cs
@@ -3,28 +3,30 @@ using DM.JSON;
 using DM.REST;
 using System;
 using Xamarin.Forms;
-using DM.Views;
 using System.IO;
 using System.Collections.Generic;
-using System.Reflection;
-using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 using DM.ViewClassesToDisplay;
 using DM.DB;
 using SQLite;
+using DM.ViewClasses;
 
 namespace DM.ViewModels
 {
-    class WelcomeViewModel
+    class ProfileViewModel
     {
-        public WelcomeViewModel()
+        public ProfileViewModel()
         {
             RestService rest = RestService.Instance;
             Profile = rest.GetPlayerInfo(Id_holder.Instance.Steam32id);
             Winloses = rest.GetWLInfo(Id_holder.Instance.Steam32id + "/wl");
             MostPlayedHeroes = rest.GetPlayerMostPlayedHeroes(Id_holder.Instance.Steam32id + "/heroes");
+            RecentMatches = rest.GetPlayerRecentMatches(Id_holder.Instance.Steam32id + "/recentMatches"); ;
+            InitializeRecentMatches();
             InitializeMostPlayedHeroes();
         }
+        public List<MatchCropped> RecentMatches { get; set; }
+        public ObservableCollection<RecentMatchToDisplay> RecentMacthesToDisplay { get; } = new ObservableCollection<RecentMatchToDisplay>();
 
         public ProfileObject Profile { get; set; }
         public ImageSource Avatar
@@ -37,14 +39,6 @@ namespace DM.ViewModels
         public List<MostPlayedHero> MostPlayedHeroes { get; set; }
 
         public ObservableCollection<MostPlayedHeroToDisplay> MostPlayedHeroesToDisplay { get; } = new ObservableCollection<MostPlayedHeroToDisplay>();
-
-        public Command ShowRecentMatchesCommand => new Command(ShowRecentMatches);
-
-        private void ShowRecentMatches()
-        {
-            App.Current.MainPage.Navigation.PushAsync(new RecentMatchesPage());
-            
-        }
 
         private HeroesDB GetHero(int heroID)
         {
@@ -61,6 +55,14 @@ namespace DM.ViewModels
             foreach (MostPlayedHero hero in MostPlayedHeroes)
             {
                 MostPlayedHeroesToDisplay.Add(new MostPlayedHeroToDisplay(hero, GetHero(Convert.ToInt32(hero.hero_id))));
+            }
+        }
+
+        private void InitializeRecentMatches()
+        {
+            foreach (MatchCropped match in RecentMatches)
+            {
+                RecentMacthesToDisplay.Add(new RecentMatchToDisplay(match, GetHero(match.hero_id)));
             }
         }
     }
